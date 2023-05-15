86fff70648fb0f00b1007a9b92b06299c868e18e
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-05-15 16:34:47 -0300

    adding loading to address page

--- src/pages/Address/index.tsx
+++ src/pages/Address/index.tsx
@@ -9,6 +9,7 @@ import {
 } from './styles'
 import { MapPinLine } from 'phosphor-react'
 import { useUser } from '../../hooks/user'
+import { Loading } from '../../components/Loading'
 
 export function Address() {
   const theme = useTheme()
@@ -16,47 +17,51 @@ export function Address() {
 
   return (
     <Container>
-      {address.map((uniqueAddress) => (
-        <>
-          <Header>
-            <MapPinLine size={30} color={theme.primary} />
-            <HeaderTitle>Personal address</HeaderTitle>
-          </Header>
-          <AddressContainer>
-            <SectionAddresses className="col-1">
-              <SectionText>
-                Postal Code: {uniqueAddress.location.postcode}
-              </SectionText>
-            </SectionAddresses>
+      {!address ? (
+        <Loading />
+      ) : (
+        address.map((uniqueAddress) => (
+          <>
+            <Header>
+              <MapPinLine size={30} color={theme.primary} />
+              <HeaderTitle>Personal address</HeaderTitle>
+            </Header>
+            <AddressContainer>
+              <SectionAddresses className="col-1">
+                <SectionText>
+                  Postal Code: {uniqueAddress.location.postcode}
+                </SectionText>
+              </SectionAddresses>
 
-            <SectionAddresses className="col-2">
-              <SectionText>
-                Country: {uniqueAddress.location.country}
-              </SectionText>
-            </SectionAddresses>
+              <SectionAddresses className="col-2">
+                <SectionText>
+                  Country: {uniqueAddress.location.country}
+                </SectionText>
+              </SectionAddresses>
 
-            <SectionAddresses className="col-3">
-              <SectionText>
-                Street Name: {uniqueAddress.location.street.name}
-              </SectionText>
-            </SectionAddresses>
+              <SectionAddresses className="col-3">
+                <SectionText>
+                  Street Name: {uniqueAddress.location.street.name}
+                </SectionText>
+              </SectionAddresses>
 
-            <SectionAddresses className="col-1">
-              <SectionText>
-                Number: {uniqueAddress.location.street.number}
-              </SectionText>
-            </SectionAddresses>
+              <SectionAddresses className="col-1">
+                <SectionText>
+                  Number: {uniqueAddress.location.street.number}
+                </SectionText>
+              </SectionAddresses>
 
-            <SectionAddresses className="col-1">
-              <SectionText>City: {uniqueAddress.location.city}</SectionText>
-            </SectionAddresses>
+              <SectionAddresses className="col-1">
+                <SectionText>City: {uniqueAddress.location.city}</SectionText>
+              </SectionAddresses>
 
-            <SectionAddresses className="col-1">
-              <SectionText>State: {uniqueAddress.location.state}</SectionText>
-            </SectionAddresses>
-          </AddressContainer>
-        </>
-      ))}
+              <SectionAddresses className="col-1">
+                <SectionText>State: {uniqueAddress.location.state}</SectionText>
+              </SectionAddresses>
+            </AddressContainer>
+          </>
+        ))
+      )}
     </Container>
   )
 }
